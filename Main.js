b2886529c2cbf7073c2ce841f0f870e64f5ccdd7
박건우 <rjsdn0998@gmail.com>
2023-06-14 20:36:11 +0900

    이산화탄소 계산기
    3일차

---
+++ Main.js
@@ -0,0 +1,6 @@
+function checked() {
+  const checkbox2 = document.getElementById("fuel-checkbox2");
+  checkbox2.addEventListener("click", function () {
+    checkbox2.style.color = "blue";
+  });
+}
