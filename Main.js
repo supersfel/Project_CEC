29793bbc7dce3b005635681e12bb922992afca09
박건우 <rjsdn0998@gmail.com>
2023-07-03 16:21:34 +0900

    [Add] 그래프
    결과값과 평균값을 비교하는 그래프 추가

--- Main.js
+++ Main.js
@@ -3,11 +3,10 @@ const $fuelCheckbox2 = document.querySelector(".fuel-checkbox2");
 const $resultButtonBackground = document.querySelector(
   ".result-button-background"
 );
-
 let fuel = 1; //연료 종류
 let distance = 0; //이동 거리
 let fuelEfficiency = 0; //연비
-let Co2Amount = 0; //이산화탄소 배출량
+let Co2Amount = 0;
 
 //경유 체크박스를 클릭했을 때
 $fuelCheckbox1.addEventListener("click", function (e) {
@@ -23,13 +22,6 @@ $fuelCheckbox2.addEventListener("click", function (e) {
   fuel = 2;
 });
 
-//이동거리와 연비 입력값 불러오기
-function valueBring() {
-  distance = document.getElementById("distance").value;
-  distance = distance.replace(/,/g, "");
-  fuelEfficiency = document.getElementById("fuel-efficiency").value;
-}
-
 //결과 버튼을 클릭했을 때
 $resultButtonBackground.addEventListener("click", function (e) {
   let link = "Result.html"; //결과창 주소
@@ -40,6 +32,13 @@ $resultButtonBackground.addEventListener("click", function (e) {
     console.log(Co2Amount);
   }
 });
+
+//이동거리와 연비 입력값 불러오기
+function valueBring() {
+  distance = document.getElementById("distance").value;
+  distance = distance.replace(/,/g, "");
+  fuelEfficiency = document.getElementById("fuel-efficiency").value;
+}
 
 //이산화탄소 발생량 계산기 식
 function Co2EmissionsCalculate(fuel, distance, fuelEfficiency) {
@@ -58,6 +57,7 @@ function Co2EmissionsCalculate(fuel, distance, fuelEfficiency) {
   }
 
   fuelUsage = distance / fuelEfficiency; //연료 사용량 계산식
-  Co2Amount = (fuelUsage * fever * emissionsFactor * (44 / 12) * 1).toFixed(2); //이산화탄소 배출량 계산식
+  Co2Amount = fuelUsage * fever * emissionsFactor * (44 / 12) * 1; //이산화탄소 배출량 계산식
   Co2Amount = Math.round(Co2Amount * 10) / 10; //소수 첫째 자리까지 반올림
+  localStorage.setItem("Co2A", JSON.stringify(Co2Amount));
 }
--- Result_System.js
+++ Result_System.js
@@ -1,25 +1,48 @@
-new Chart(document.querySelector("result-graph"), {
-  type: "bar",
+let Co2Amount = JSON.parse(localStorage.getItem("Co2A"));
+var context = document.getElementById("myChart").getContext("2d");
+const $resultContents = document.getElementById("result-contents");
+var text = Co2Amount;
+
+$resultContents.innerHTML = text;
+
+var myChart = new Chart(context, {
+  type: "bar", // 차트의 형태
   data: {
+    // 차트에 들어갈 데이터
+    labels: [
+      //x 축
+      "다른 사람",
+      "나",
+    ],
     datasets: [
       {
-        label: "Population (millions)",
+        //데이터
+        label: "이산화탄소 배출량 그래프", //차트 제목
+        fill: false, // line 형태일 때, 선 안쪽을 채우는지 안채우는지
+        data: [270.8, Co2Amount],
         backgroundColor: [
-          "#3e95cd",
-          "#8e5ea2",
-          "#3cba9f",
-          "#e8c3b9",
-          "#c45850",
+          //색상
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
         ],
-        data: [2478, 5267, 734, 784, 433],
+        borderColor: [
+          //경계선 색상
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+        ],
+        borderWidth: 1, //경계선 굵기
       },
     ],
   },
   options: {
-    legend: { display: false },
-    title: {
-      display: true,
-      text: "Predicted world population (millions) in 2050",
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
     },
   },
 });
